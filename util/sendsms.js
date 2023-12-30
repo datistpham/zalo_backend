b60916c99b0026853d398d60bd37ddc36256c199
datistpham <78075406+datistpham@users.noreply.github.com>
2023-12-30 23:02:48 +0700

    Update sendsms.js

--- util/sendsms.js
+++ util/sendsms.js
@@ -4,12 +4,12 @@ const sendMail =async (email, codeVerify)=>{
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "vansangdang2k1@gmail.com",
-        pass: "kamayxykaakmqqux",
+        user: "anh_2051220137@dau.edu.vn",
+        pass: "pmriwwhmidhyzocp",
     },
     });
 const mailOptions = {
-from: "vansangdang2k1@gmail.com",
+from: "anh_2051220137@dau.edu.vn",
 to: email,
 subject: "Verification code",
 html: `<div><div>Đây là mã xác thực thay đổi mật khẩu của bạn, Vui lòng không chia sẻ cho bất cứ ai, Mã sẽ hết hạn trong vòng 3 phút</div>
